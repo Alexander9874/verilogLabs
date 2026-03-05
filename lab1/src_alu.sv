2b2c9470dd48aaacadccfd1fcff0e97a28d66626
Alexander <sashasereda93@gmail.com>
2026-03-05 06:55:04 -0500

    lab1: update tb; combine some moduls in src

--- lab1/src_alu.sv
+++ lab1/src_alu.sv
@@ -19,41 +19,9 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-//module OR #(parameter WIDTH = 8)(
-//    input  logic [WIDTH-1:0] A,
-//    input  logic [WIDTH-1:0] B,
-//    output logic [WIDTH-1:0] R
-//    );
-//    assign R = A | B;
-//endmodule
-
-//module AND #(parameter WIDTH = 8)(
-//    input  logic [WIDTH-1:0] A,
-//    input  logic [WIDTH-1:0] B,
-//    output logic [WIDTH-1:0] R
-//    );
-//    assign R = A & B;
-//endmodule
-
-//module SLTU #(parameter WIDTH = 8)(
-//    input  logic [WIDTH-1:0] A,
-//    input  logic [WIDTH-1:0] B,
-//    output logic [WIDTH-1:0] R
-//    );
-//    assign R = (A < B) ? 1 : 0;
-//endmodule
-
-//module ADD #(parameter WIDTH = 8)(
-//    input  logic [WIDTH-1:0] A,
-//    input  logic [WIDTH-1:0] B,
-//    output logic [WIDTH-1:0] R
-//    );
-//    assign R = A + B;
-//endmodule
-
 module ADD #(parameter WIDTH = 8)(
-    input  logic [WIDTH-1:0] A,
-    input  logic [WIDTH-1:0] B,
+    input logic [WIDTH-1:0] A,
+    input logic [WIDTH-1:0] B,
     input logic C,
     output logic [WIDTH-1:0] R
     );
@@ -67,30 +35,9 @@ module ADD #(parameter WIDTH = 8)(
     end
 endmodule
 
-module SUB #(parameter WIDTH = 8)(
-    input  logic [WIDTH-1:0] A,
-    input  logic [WIDTH-1:0] B,
-    output logic [WIDTH-1:0] R
-    );
-    ADD #(.WIDTH(WIDTH)) add (
-        .A (A),
-        .B (~B),
-        .C (1),
-        .R (R)
-    );
-endmodule
-
-module XOR #(parameter WIDTH = 8)(
-    input  logic [WIDTH-1:0] A,
-    input  logic [WIDTH-1:0] B,
-    output logic [WIDTH-1:0] R
-    );
-    assign R = A ^ B;
-endmodule
-
 module SLL #(parameter WIDTH = 8)(
-    input  logic [WIDTH-1:0] A,
-    input  logic [WIDTH-1:0] B,
+    input logic [WIDTH-1:0] A,
+    input logic [WIDTH-1:0] B,
     output logic [WIDTH-1:0] R
     );
     always_comb begin
@@ -101,46 +48,17 @@ module SLL #(parameter WIDTH = 8)(
     end
 endmodule
 
-module SRL #(parameter WIDTH = 8)(
-    input  logic [WIDTH-1:0] A,
-    input  logic [WIDTH-1:0] B,
+// A[WIDTH] ? SRA : SRL
+module SRX #(parameter WIDTH = 8)(
+    input logic [WIDTH:0] A,
+    input logic [WIDTH-1:0] B,
     output logic [WIDTH-1:0] R
     );
     always_comb begin
         if (B >= WIDTH)
-            R = 0;
-        else
-            R = A >> B;
-    end
-endmodule
-
-module SRA #(parameter WIDTH = 8)(
-    input  logic [WIDTH-1:0] A,
-    input  logic [WIDTH-1:0] B,
-    output logic [WIDTH-1:0] R
-    );
-    always_comb begin
-        if (B >= WIDTH)
-            R = {WIDTH{A[WIDTH-1]}};
+            R = {WIDTH{A[WIDTH]}};
         else
             R = $signed(A) >>> B;
-    end
-endmodule
-
-module SLT #(parameter WIDTH = 8)(
-    input  logic [WIDTH-1:0] A,
-    input  logic [WIDTH-1:0] B,
-    output logic [WIDTH-1:0] R
-    );
-    always_comb begin
-        unique case ({A[WIDTH-1], B[WIDTH-1]})
-            2'b01:
-                R = 0;
-            2'b10:
-                R = 1;
-            default:
-                R = (A < B) ? 1 : 0;
-        endcase
     end
 endmodule
 
@@ -152,44 +70,27 @@ module ALU #(parameter WIDTH = 8)(
     output logic ZF
 );
     logic [WIDTH-1:0] R_ADD;
-    logic [WIDTH-1:0] R_SUB;
-    logic [WIDTH-1:0] R_XOR;
-    logic [WIDTH-1:0] R_OR;
-    logic [WIDTH-1:0] R_AND;
     logic [WIDTH-1:0] R_SLL;
-    logic [WIDTH-1:0] R_SRL;
-    logic [WIDTH-1:0] R_SRA;
-    logic [WIDTH-1:0] R_SLT;
-    logic [WIDTH-1:0] R_SLTU;
+    logic [WIDTH-1:0] R_SRX;
 
-    ADD  #(WIDTH) _add  (.A(A), .B(B), .C(0), .R(R_ADD));
-    SUB  #(WIDTH) _sub  (.A(A), .B(B), .R(R_SUB));
+    ADD  #(WIDTH) _add  (.A(A), .B(OPCODE[3] ? ~B : B), .C(OPCODE[3]), .R(R_ADD));
     XOR  #(WIDTH) _xor  (.A(A), .B(B), .R(R_XOR));
-//    OR   #(WIDTH) _or   (.A(A), .B(B), .R(R_OR));
-//    AND  #(WIDTH) _and  (.A(A), .B(B), .R(R_AND));
     SLL  #(WIDTH) _sll  (.A(A), .B(B), .R(R_SLL));
-    SRL  #(WIDTH) _srl  (.A(A), .B(B), .R(R_SRL));
-    SRA  #(WIDTH) _sra  (.A(A), .B(B), .R(R_SRA));
-    SLT  #(WIDTH) _slt  (.A(A), .B(B), .R(R_SLT));
-//    SLTU #(WIDTH) _sltu (.A(A), .B(B), .R(R_SLTU));
+    SRX  #(WIDTH) _srx  (.A({OPCODE[3] & A[WIDTH-1], A}), .B(B), .R(R_SRX));
 
     always_comb begin
-        unique case (OPCODE)
-            4'b0000: R = R_ADD;
-            4'b1000: R = R_SUB;
-            4'b0100: R = R_XOR;
-//            4'b0110: R = R_OR;
-//            4'b0111: R = R_AND;
+        unique casex (OPCODE)    
+            4'bx000: R = R_ADD;
+            4'b0100: R = A ^ B;
             4'b0110: R = A | B;
             4'b0111: R = A & B;
             4'b0001: R = R_SLL;
-            4'b0101: R = R_SRL;
-            4'b1101: R = R_SRA;
-            4'b0010: R = R_SLT;
-//            4'b0011: R = R_SLTU;
+            4'bx101: R = R_SRX;
+            4'b0010: R = ($signed(A) < $signed(B)) ? 1 : 0;
             4'b0011: R = (A < B) ? 1 : 0;
             default: R = 0;
         endcase
     end
+    
     assign ZF = (R == 0);
 endmodule
--- lab1/test_bench_alu.sv
+++ lab1/test_bench_alu.sv
@@ -22,6 +22,11 @@
 module TB_ALU;
     localparam WIDTH = 4;
 
+    const logic [3:0] opcode_array [9:0] = '{4'b0000, 4'b1000,
+                                             4'b0100, 4'b0110,
+                                             4'b0111, 4'b0001,
+                                             4'b0101, 4'b1101,
+                                             4'b0010, 4'b0011};
     logic [WIDTH-1:0] A;
     logic [WIDTH-1:0] B;
     logic [WIDTH-1:0] R;
@@ -38,55 +43,51 @@ module TB_ALU;
     
     initial begin
         $display("ALU TEST BEGIN: WIDTH = %0d", WIDTH);
-        A <= 4;
-        B <= 2;
-        OPCODE = 4'b0000;
-        #10;
-        OPCODE = 4'b1000;
-        #10;
-        OPCODE = 4'b0100;
-        #10;
-        OPCODE = 4'b0110;
-        #10;
-        OPCODE = 4'b0111;
-        #10;
-        OPCODE = 4'b0001;
-        #10;
-        OPCODE = 4'b0101;
-        #10;
-        OPCODE = 4'b1101;
-        #10;
-        OPCODE = 4'b0010;
-        #10;
-        OPCODE = 4'b0011;
-        #10;
-        
-        A <= 13;
-        B <= 1;
-        OPCODE = 4'b0000;
-        #10;
-        OPCODE = 4'b1000;
-        #10;
-        OPCODE = 4'b0100;
-        #10;
-        OPCODE = 4'b0110;
-        #10;
-        OPCODE = 4'b0111;
-        #10;
-        OPCODE = 4'b0001;
-        #10;
-        OPCODE = 4'b0101;
-        #10;
-        OPCODE = 4'b1101;
-        #10;
-        OPCODE = 4'b0010;
-        #10;
-        OPCODE = 4'b0011;
-        #10;
-        
-        OPCODE = 4'B1111;
-        #10;
-        
-        $display("TEST EDN");
+        for(int i = 0; i < 10; i++) begin
+            OPCODE = opcode_array[i];
+            for(int j = 0; j < 2**(WIDTH) - 1; j++) begin
+                for(int k = 0; k < 2**(WIDTH) - 1; k++) begin
+                    A <= j;
+                    B <= k;
+                    #10;
+                    unique case (OPCODE)
+                        4'b0000:
+                            assert ($signed(R) == ($signed(A) + $signed(B)))
+                            else $error("\tOPCODE = %b (ADD)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, $signed(A) + $signed(B));
+                        4'b1000:
+                            assert ($signed(R) == ($signed(A) - $signed(B)))
+                            else $error("\tOPCODE = %b (SUB)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, $signed(A) - $signed(B));
+                        4'b0100:
+                            assert (R == (A ^ B))
+                            else $error("\tOPCODE = %b (XOR)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, A ^ B);
+                        4'b0110:
+                            assert (R == (A | B))
+                            else $error("\tOPCODE = %b (OR)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, A | B);
+                        4'b0111:
+                            assert (R == (A & B))
+                            else $error("\tOPCODE = %b (AND)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, A & B);
+                        4'b0001:
+                            assert (R == (A << B))
+                            else $error("\tOPCODE = %b (SLL)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, A << B);
+                        4'b0101:
+                            assert (R == (A >> B))
+                            else $error("\tOPCODE = %b (SRL)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, A >> B);
+                        4'b1101:
+                            assert ($signed(R) == ($signed(A) >>> B))
+                            else $error("\tOPCODE = %b (SRA)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, $signed(A) >>> B);
+                        4'b0010:
+                            assert (R == (($signed(A) < $signed(B)) ? 1 : 0))
+                            else $error("\tOPCODE = %b (SLT)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, ($signed(A) < $signed(B)) ? 1 : 0);
+                        4'b0011:
+                            assert (R == ((A < B) ? 1 : 0))
+                            else $error("\tOPCODE = %b (SLTU)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, (A < B) ? 1 : 0);
+                        default:
+                            assert (R == 0)
+                            else $error("\tOPCODE = %b (NONE)\tA = %b\tB = %b\tR = %b (%b)\n", OPCODE, A, B, R, 0);
+                    endcase
+                end
+            end
+        end
+        $display("TEST END");
     end
 endmodule
